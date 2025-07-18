9871676789dc6876fdf2604ae20de48a3e670993
NicoCampa <nicolocampagnoli20@icloud.com>
2025-07-18 22:36:54 +0200

    Initial Commit

---
+++ aWordaDay/aWordaDayApp.swift
@@ -0,0 +1,32 @@
+//
+//  aWordaDayApp.swift
+//  aWordaDay
+//
+//  Created by Nicolò Campagnoli on 18.07.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct aWordaDayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ aWordaDayTests/aWordaDayTests.swift
@@ -0,0 +1,16 @@
+//
+//  aWordaDayTests.swift
+//  aWordaDayTests
+//
+//  Created by Nicolò Campagnoli on 18.07.25.
+//
+
+import Testing
+
+struct aWordaDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
